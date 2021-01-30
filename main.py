31a068f070a9535f1c57135ba7208dbbce30484e
obaidsam <75863047+obaidsam@users.noreply.github.com>
2021-01-30 15:11:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+password = " "
+while password != "sameer":
+  password = input("Enter the Password: ")
+  if password == "sameer":
+    print("Congrats you enter the Correct password")
+  else:
+    print("PLease Try again")
